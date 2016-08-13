64e28c7852aedf26995e346523f4712309075eb8
Benjamin Garrett <bgarrett@temple.edu>
2016-08-13 16:32:04 -0400

    Setup project on new laptop

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -21,7 +21,8 @@ FC=gfortran
 AS=as
 
 # Macros
-CND_PLATFORM=GNU-Linux-x86
+CND_PLATFORM=GNU-Linux
+CND_DLIB_EXT=so
 CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
@@ -34,12 +35,12 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/_ext/982968635/MurmurHash32.o \
-	${OBJECTDIR}/memo_command_interpreter.o \
-	${OBJECTDIR}/_ext/982968635/MurmurHash64A.o \
-	${OBJECTDIR}/_ext/1431108635/memo.o \
-	${OBJECTDIR}/_ext/853948430/random.o \
-	${OBJECTDIR}/main.o
+	${OBJECTDIR}/_ext/c56916c5/MurmurHash32.o \
+	${OBJECTDIR}/_ext/c56916c5/MurmurHash64A.o \
+	${OBJECTDIR}/_ext/554cfc1b/memo.o \
+	${OBJECTDIR}/_ext/cd19c7f2/random.o \
+	${OBJECTDIR}/main.o \
+	${OBJECTDIR}/memo_command_interpreter.o
 
 # Test Directory
 TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
@@ -53,6 +54,16 @@ TESTFILES= \
 	${TESTDIR}/TestFiles/f5 \
 	${TESTDIR}/TestFiles/f3 \
 	${TESTDIR}/TestFiles/f1
+
+# Test Object Files
+TESTOBJECTFILES= \
+	${TESTDIR}/tests/cuckoo_hashing_test_3_byte_key_9_byte_value.o \
+	${TESTDIR}/tests/cuckoo_hashing_test_fill_table.o \
+	${TESTDIR}/tests/generate_artificial_operation_sequence_feedback.o \
+	${TESTDIR}/tests/generate_operation_sequence_without_feedback.o \
+	${TESTDIR}/tests/hashing_test_read_write_delete.o \
+	${TESTDIR}/tests/linear_probe_test_3_byte_key_9_byte_value.o \
+	${TESTDIR}/tests/linear_probe_test_fill_table.o
 
 # C Compiler Flags
 CFLAGS=
@@ -76,43 +87,45 @@ LDLIBSOPTIONS=
 
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/memo100: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/memo100 ${OBJECTFILES} ${LDLIBSOPTIONS} 
+	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/memo100 ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/_ext/982968635/MurmurHash32.o: ../hashing/MurmurHash32.c 
-	${MKDIR} -p ${OBJECTDIR}/_ext/982968635
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/982968635/MurmurHash32.o ../hashing/MurmurHash32.c
+${OBJECTDIR}/_ext/c56916c5/MurmurHash32.o: ../hashing/MurmurHash32.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/c56916c5
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/c56916c5/MurmurHash32.o ../hashing/MurmurHash32.c
 
-${OBJECTDIR}/memo_command_interpreter.o: memo_command_interpreter.c 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -MMD -MP -MF $@.d -o ${OBJECTDIR}/memo_command_interpreter.o memo_command_interpreter.c
+${OBJECTDIR}/_ext/c56916c5/MurmurHash64A.o: ../hashing/MurmurHash64A.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/c56916c5
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/c56916c5/MurmurHash64A.o ../hashing/MurmurHash64A.c
 
-${OBJECTDIR}/_ext/982968635/MurmurHash64A.o: ../hashing/MurmurHash64A.c 
-	${MKDIR} -p ${OBJECTDIR}/_ext/982968635
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/982968635/MurmurHash64A.o ../hashing/MurmurHash64A.c
+${OBJECTDIR}/_ext/554cfc1b/memo.o: ../memoization/memo.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/554cfc1b
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/554cfc1b/memo.o ../memoization/memo.c
 
-${OBJECTDIR}/_ext/1431108635/memo.o: ../memoization/memo.c 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1431108635
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1431108635/memo.o ../memoization/memo.c
-
-${OBJECTDIR}/_ext/853948430/random.o: ../random/random.c 
-	${MKDIR} -p ${OBJECTDIR}/_ext/853948430
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/853948430/random.o ../random/random.c
+${OBJECTDIR}/_ext/cd19c7f2/random.o: ../random/random.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/cd19c7f2
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/cd19c7f2/random.o ../random/random.c
 
 ${OBJECTDIR}/main.o: main.c 
 	${MKDIR} -p ${OBJECTDIR}
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.c
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.c
+
+${OBJECTDIR}/memo_command_interpreter.o: memo_command_interpreter.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/memo_command_interpreter.o memo_command_interpreter.c
 
 # Subprojects
 .build-subprojects:
 
 # Build Test Targets
-.build-tests-conf: .build-conf ${TESTFILES}
+.build-tests-conf: .build-tests-subprojects .build-conf ${TESTFILES}
+.build-tests-subprojects:
+
 ${TESTDIR}/TestFiles/f4: ${TESTDIR}/tests/cuckoo_hashing_test_3_byte_key_9_byte_value.o ${OBJECTFILES:%.o=%_nomain.o}
 	${MKDIR} -p ${TESTDIR}/TestFiles
 	${LINK.c}   -o ${TESTDIR}/TestFiles/f4 $^ ${LDLIBSOPTIONS} 
@@ -144,109 +157,96 @@ ${TESTDIR}/TestFiles/f1: ${TESTDIR}/tests/linear_probe_test_fill_table.o ${OBJEC
 
 ${TESTDIR}/tests/cuckoo_hashing_test_3_byte_key_9_byte_value.o: tests/cuckoo_hashing_test_3_byte_key_9_byte_value.c 
 	${MKDIR} -p ${TESTDIR}/tests
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -I. -MMD -MP -MF $@.d -o ${TESTDIR}/tests/cuckoo_hashing_test_3_byte_key_9_byte_value.o tests/cuckoo_hashing_test_3_byte_key_9_byte_value.c
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/cuckoo_hashing_test_3_byte_key_9_byte_value.o tests/cuckoo_hashing_test_3_byte_key_9_byte_value.c
 
 
 ${TESTDIR}/tests/cuckoo_hashing_test_fill_table.o: tests/cuckoo_hashing_test_fill_table.c 
 	${MKDIR} -p ${TESTDIR}/tests
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -I. -MMD -MP -MF $@.d -o ${TESTDIR}/tests/cuckoo_hashing_test_fill_table.o tests/cuckoo_hashing_test_fill_table.c
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/cuckoo_hashing_test_fill_table.o tests/cuckoo_hashing_test_fill_table.c
 
 
 ${TESTDIR}/tests/generate_artificial_operation_sequence_feedback.o: tests/generate_artificial_operation_sequence_feedback.c 
 	${MKDIR} -p ${TESTDIR}/tests
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -I. -MMD -MP -MF $@.d -o ${TESTDIR}/tests/generate_artificial_operation_sequence_feedback.o tests/generate_artificial_operation_sequence_feedback.c
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/generate_artificial_operation_sequence_feedback.o tests/generate_artificial_operation_sequence_feedback.c
 
 
 ${TESTDIR}/tests/generate_operation_sequence_without_feedback.o: tests/generate_operation_sequence_without_feedback.c 
 	${MKDIR} -p ${TESTDIR}/tests
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -I. -MMD -MP -MF $@.d -o ${TESTDIR}/tests/generate_operation_sequence_without_feedback.o tests/generate_operation_sequence_without_feedback.c
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/generate_operation_sequence_without_feedback.o tests/generate_operation_sequence_without_feedback.c
 
 
 ${TESTDIR}/tests/hashing_test_read_write_delete.o: tests/hashing_test_read_write_delete.c 
 	${MKDIR} -p ${TESTDIR}/tests
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -I. -MMD -MP -MF $@.d -o ${TESTDIR}/tests/hashing_test_read_write_delete.o tests/hashing_test_read_write_delete.c
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/hashing_test_read_write_delete.o tests/hashing_test_read_write_delete.c
 
 
 ${TESTDIR}/tests/linear_probe_test_3_byte_key_9_byte_value.o: tests/linear_probe_test_3_byte_key_9_byte_value.c 
 	${MKDIR} -p ${TESTDIR}/tests
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -I. -MMD -MP -MF $@.d -o ${TESTDIR}/tests/linear_probe_test_3_byte_key_9_byte_value.o tests/linear_probe_test_3_byte_key_9_byte_value.c
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/linear_probe_test_3_byte_key_9_byte_value.o tests/linear_probe_test_3_byte_key_9_byte_value.c
 
 
 ${TESTDIR}/tests/linear_probe_test_fill_table.o: tests/linear_probe_test_fill_table.c 
 	${MKDIR} -p ${TESTDIR}/tests
-	${RM} $@.d
-	$(COMPILE.c) -g -I. -I. -MMD -MP -MF $@.d -o ${TESTDIR}/tests/linear_probe_test_fill_table.o tests/linear_probe_test_fill_table.c
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/linear_probe_test_fill_table.o tests/linear_probe_test_fill_table.c
 
 
-${OBJECTDIR}/_ext/982968635/MurmurHash32_nomain.o: ${OBJECTDIR}/_ext/982968635/MurmurHash32.o ../hashing/MurmurHash32.c 
-	${MKDIR} -p ${OBJECTDIR}/_ext/982968635
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/982968635/MurmurHash32.o`; \
+${OBJECTDIR}/_ext/c56916c5/MurmurHash32_nomain.o: ${OBJECTDIR}/_ext/c56916c5/MurmurHash32.o ../hashing/MurmurHash32.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/c56916c5
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/c56916c5/MurmurHash32.o`; \
 	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/982968635/MurmurHash32_nomain.o ../hashing/MurmurHash32.c;\
+	    ${RM} "$@.d";\
+	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/c56916c5/MurmurHash32_nomain.o ../hashing/MurmurHash32.c;\
 	else  \
-	    ${CP} ${OBJECTDIR}/_ext/982968635/MurmurHash32.o ${OBJECTDIR}/_ext/982968635/MurmurHash32_nomain.o;\
+	    ${CP} ${OBJECTDIR}/_ext/c56916c5/MurmurHash32.o ${OBJECTDIR}/_ext/c56916c5/MurmurHash32_nomain.o;\
 	fi
 
-${OBJECTDIR}/memo_command_interpreter_nomain.o: ${OBJECTDIR}/memo_command_interpreter.o memo_command_interpreter.c 
-	${MKDIR} -p ${OBJECTDIR}
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/memo_command_interpreter.o`; \
+${OBJECTDIR}/_ext/c56916c5/MurmurHash64A_nomain.o: ${OBJECTDIR}/_ext/c56916c5/MurmurHash64A.o ../hashing/MurmurHash64A.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/c56916c5
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/c56916c5/MurmurHash64A.o`; \
 	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/memo_command_interpreter_nomain.o memo_command_interpreter.c;\
+	    ${RM} "$@.d";\
+	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/c56916c5/MurmurHash64A_nomain.o ../hashing/MurmurHash64A.c;\
 	else  \
-	    ${CP} ${OBJECTDIR}/memo_command_interpreter.o ${OBJECTDIR}/memo_command_interpreter_nomain.o;\
+	    ${CP} ${OBJECTDIR}/_ext/c56916c5/MurmurHash64A.o ${OBJECTDIR}/_ext/c56916c5/MurmurHash64A_nomain.o;\
 	fi
 
-${OBJECTDIR}/_ext/982968635/MurmurHash64A_nomain.o: ${OBJECTDIR}/_ext/982968635/MurmurHash64A.o ../hashing/MurmurHash64A.c 
-	${MKDIR} -p ${OBJECTDIR}/_ext/982968635
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/982968635/MurmurHash64A.o`; \
+${OBJECTDIR}/_ext/554cfc1b/memo_nomain.o: ${OBJECTDIR}/_ext/554cfc1b/memo.o ../memoization/memo.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/554cfc1b
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/554cfc1b/memo.o`; \
 	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/982968635/MurmurHash64A_nomain.o ../hashing/MurmurHash64A.c;\
+	    ${RM} "$@.d";\
+	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/554cfc1b/memo_nomain.o ../memoization/memo.c;\
 	else  \
-	    ${CP} ${OBJECTDIR}/_ext/982968635/MurmurHash64A.o ${OBJECTDIR}/_ext/982968635/MurmurHash64A_nomain.o;\
+	    ${CP} ${OBJECTDIR}/_ext/554cfc1b/memo.o ${OBJECTDIR}/_ext/554cfc1b/memo_nomain.o;\
 	fi
 
-${OBJECTDIR}/_ext/1431108635/memo_nomain.o: ${OBJECTDIR}/_ext/1431108635/memo.o ../memoization/memo.c 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1431108635
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/1431108635/memo.o`; \
+${OBJECTDIR}/_ext/cd19c7f2/random_nomain.o: ${OBJECTDIR}/_ext/cd19c7f2/random.o ../random/random.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/cd19c7f2
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/cd19c7f2/random.o`; \
 	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1431108635/memo_nomain.o ../memoization/memo.c;\
+	    ${RM} "$@.d";\
+	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/cd19c7f2/random_nomain.o ../random/random.c;\
 	else  \
-	    ${CP} ${OBJECTDIR}/_ext/1431108635/memo.o ${OBJECTDIR}/_ext/1431108635/memo_nomain.o;\
-	fi
-
-${OBJECTDIR}/_ext/853948430/random_nomain.o: ${OBJECTDIR}/_ext/853948430/random.o ../random/random.c 
-	${MKDIR} -p ${OBJECTDIR}/_ext/853948430
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/_ext/853948430/random.o`; \
-	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
-	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/853948430/random_nomain.o ../random/random.c;\
-	else  \
-	    ${CP} ${OBJECTDIR}/_ext/853948430/random.o ${OBJECTDIR}/_ext/853948430/random_nomain.o;\
+	    ${CP} ${OBJECTDIR}/_ext/cd19c7f2/random.o ${OBJECTDIR}/_ext/cd19c7f2/random_nomain.o;\
 	fi
 
 ${OBJECTDIR}/main_nomain.o: ${OBJECTDIR}/main.o main.c 
@@ -256,10 +256,23 @@ ${OBJECTDIR}/main_nomain.o: ${OBJECTDIR}/main.o main.c
 	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/main_nomain.o main.c;\
+	    ${RM} "$@.d";\
+	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main_nomain.o main.c;\
 	else  \
 	    ${CP} ${OBJECTDIR}/main.o ${OBJECTDIR}/main_nomain.o;\
+	fi
+
+${OBJECTDIR}/memo_command_interpreter_nomain.o: ${OBJECTDIR}/memo_command_interpreter.o memo_command_interpreter.c 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/memo_command_interpreter.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.c) -g -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/memo_command_interpreter_nomain.o memo_command_interpreter.c;\
+	else  \
+	    ${CP} ${OBJECTDIR}/memo_command_interpreter.o ${OBJECTDIR}/memo_command_interpreter_nomain.o;\
 	fi
 
 # Run Test Targets
